b960fb6ded6a74ccc3e3da983099fcd6c937a653
Nnabugwu Michael Uchechukwu <nnabugwu.michael@gmail.com>
2024-08-31 17:25:53 +0100

    added API doc using swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 5555;
+
+  // Setup Swagger for API docs
+  const config = new DocumentBuilder()
+    .setTitle('AutoChek Loan Application API')
+    .setDescription('API documentation for AutoChek Loan Application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port);
 }
