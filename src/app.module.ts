e9c4c3ace7b7e1e832c5e18c7efbc11ecf9dbe7f
Nnabugwu Michael Uchechukwu <nnabugwu.michael@gmail.com>
2024-08-31 09:42:03 +0100

    defined user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Vehicle } from './entities/vehicle.entity';
+import { LoanApplication } from './entities/loan-application.entity';
+import { Valuation } from './entities/valuation.entity';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'sqlite',
+        database: ':memory:',
+        entities: [User, Vehicle, LoanApplication, Valuation],
+        synchronize: true,
+      }
+    ),
+    TypeOrmModule.forFeature([User, Vehicle, LoanApplication, Valuation]),
+    VehicleModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {}
