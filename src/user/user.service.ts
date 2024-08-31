662eba5084bf9886110201bc5505b40cc527da27
Nnabugwu Michael Uchechukwu <nnabugwu.michael@gmail.com>
2024-08-31 09:53:10 +0100

    defined create user method in services

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const { username, email, password } = createUserDto;
+    
+        // Hash the password before saving
+        const hashedPassword = await bcrypt.hash(password, 10);
+    
+        const user = this.userRepository.create({
+          username,
+          email,
+          password: hashedPassword,
+        });
+    
+        return this.userRepository.save(user);
+      }
+}
