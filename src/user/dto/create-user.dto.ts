f60bf1c4d819c8ac264ef62dc70ee6a0c4538108
Nnabugwu Michael Uchechukwu <nnabugwu.michael@gmail.com>
2024-09-02 01:21:59 +0100

    added input validation on create user endpoint

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import { IsEmail, IsString, isString } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
     username: string;
+
+    @IsEmail({}, { message: 'Invalid email address' })
     email: string;
+
+    @IsString()
     password: string;
   }
   
